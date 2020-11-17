79e5672fb60d9191d07b0a20d8bac5574563c372
MrAdhit <47937950+AdhityaRimba@users.noreply.github.com>
2020-11-17 14:36:39 +0700

    v1.0.0

---
+++ Quick Save Saver/LoadSave.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Path = System.IO.Path;
+
+namespace Quick_Save_Saver
+{
+    /// <summary>
+    /// Interaction logic for LoadSave.xaml
+    /// </summary>
+    public partial class LoadSave : Window
+    {
+        string quicksave_path = MainWindow.savegame_path + @"\quicksave\";
+        public LoadSave()
+        {
+            InitializeComponent();
+            string[] saveList = Directory.GetFiles(quicksave_path, "*.bin");
+            foreach(string filePath in saveList)
+            {
+                string fileName = filePath.Replace(quicksave_path, "");
+                string safefileName = fileName.Replace(".bin", "");
+                Button dynamicbutton = new Button();
+                dynamicbutton.Content = safefileName;
+                loadlist_btn.Items.Add(dynamicbutton);
+                dynamicbutton.Click += (s, e) => {
+                    String real_quicksave_path = quicksave_path.Replace(@"\quicksave\", "");
+                    File.Delete(Path.Combine(real_quicksave_path, "quicksave.bin"));
+                    File.Copy(Path.Combine(quicksave_path, fileName), Path.Combine(real_quicksave_path, "quicksave.bin"));
+                    MessageBoxResult result = MessageBox.Show("Done, Click Quick Load To Load Your Save", "Confirmation", MessageBoxButton.OK, MessageBoxImage.Information);
+                    if (result == MessageBoxResult.OK)
+                        Application.Current.Shutdown();
+                };
+            }
+            
+        }
+    }
+}
---
+++ Quick Save Saver/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using Path = System.IO.Path;
+
+namespace Quick_Save_Saver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string savegame_path = "";
+        public static string quicksave_path = "";
+        bool isLoaded = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void selecetsvgm_btn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = "savegame.xml";
+            dlg.Filter = "Savegame XML file (savegame.xml)|savegame.xml";
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                isLoaded = true;
+                string savegame = dlg.FileName;
+                savegame = savegame.Replace(@"\savegame.xml", "");
+                savegame_path = savegame;
+            }
+        }
+
+        private void saveqcksve_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isLoaded == false)
+            {
+                MessageBox.Show("Please Select Your Savegame Before Saving!", "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "quicksave";
+            dlg.DefaultExt = ".bin";
+            dlg.Filter = "Binary file (.bin)|*.bin";
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string savename = dlg.SafeFileName;
+                savename = savename.Replace(".xml", ".bin");
+                string quicksave_path = savegame_path + @"\quicksave";
+                if (!Directory.Exists(quicksave_path))
+                    Directory.CreateDirectory(quicksave_path);
+                if (!File.Exists(Path.Combine(quicksave_path, savename)))
+                    File.Copy(Path.Combine(savegame_path, "quicksave.bin"), Path.Combine(quicksave_path, savename));
+            }
+        }
+
+        private void load_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isLoaded == false)
+            {
+                MessageBox.Show("Please Select Your Savegame Before Loading!", "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            LoadSave ls = new LoadSave();
+            ls.Show();
+        }
+    }
+}
